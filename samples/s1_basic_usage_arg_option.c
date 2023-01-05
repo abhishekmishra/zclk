3828fffa63b00791615d382b3efe6896ef1d4a05
Abhishek Mishra <abhishekmishra3@gmail.com>
2023-01-06 00:56:11 +0530

    add one option and one arg to the command

--- samples/s1_basic_usage_arg_option.c
+++ samples/s1_basic_usage_arg_option.c
@@ -8,7 +8,24 @@ int main(int argc, char* argv[])
 
     zclk_command *cmd = zclk_command_new(argv[0], "cmd",
                             "Basic Description", NULL);
+    
+    zclk_command_option_add(
+        cmd,
+        create_option(
+            "option-one",
+			"o", 
+            CLI_VAL_FLAG(0),
+            CLI_VAL_FLAG(0), 
+            "Option One"));
 
+    zclk_command_argument_add(
+        cmd,
+        create_argument(
+            "arg-one",
+            CLI_VAL_STRING("blah"),
+            CLI_VAL_STRING("blah"),
+            "Argument One"));
+    
     zclk_command_exec(cmd, argc, argv);
 
     printf("Created app successfully!\n");
