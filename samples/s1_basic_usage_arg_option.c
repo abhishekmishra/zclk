f601c79c2c9e975097efdb717e1b2a14c4adab25
Abhishek Mishra <abhishekmishra3@gmail.com>
2023-01-06 19:08:15 +0530

    basic handler in example

--- samples/s1_basic_usage_arg_option.c
+++ samples/s1_basic_usage_arg_option.c
@@ -1,13 +1,25 @@
 #include <cliutils.h>
 #include <stdio.h>
 
+cli_cmd_err basic_handler(void* handler_args,
+	arraylist* options, arraylist* args,
+	zclk_command_output_handler success_handler,
+	zclk_command_output_handler error_handler)
+{
+    for (size_t i = 0; i < arraylist_length(options);i++)
+    {
+        cli_option *opt = (cli_option*)arraylist_get(options, i);
+        printf("option: %s=%d\n", opt->name, opt->val->bool_value);
+    }
+}
+
 int main(int argc, char* argv[])
 {
     printf("Sample#1: Basic Usage of CLIUTILS with "
         "One Argument and One Option.\n");
 
     zclk_command *cmd = zclk_command_new(argv[0], "cmd",
-                            "Basic Description", NULL);
+                            "Basic Description", &basic_handler);
     
     zclk_command_option_add(
         cmd,
