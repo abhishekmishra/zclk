1c14c00621fea1c17bb57c59e89955f6d23f1595
Abhishek Mishra <abhishekmishra3@gmail.com>
2023-01-07 13:13:28 +0530

    format output

--- samples/s1_basic_usage_arg_option.c
+++ samples/s1_basic_usage_arg_option.c
@@ -6,19 +6,27 @@ zclk_cmd_err basic_handler(void* handler_args,
 	zclk_command_output_handler success_handler,
 	zclk_command_output_handler error_handler)
 {
+    printf("***************************************"
+        "****************************\n");
+    printf("Sample#1: Basic Usage of CLIUTILS with "
+        "One Argument and One Option.\n");
+    printf("***************************************"
+        "****************************\n");
+
+    printf("\n** Options\n");
     for (size_t i = 0; i < arraylist_length(options);i++)
     {
         zclk_option *opt = (zclk_option*)arraylist_get(options, i);
-        printf("option: %s=%d\n", opt->name, zclk_val_get_bool(opt->val));
+        printf("\toption: %s=%d\n", opt->name, zclk_val_get_bool(opt->val));
     }
+
+    printf("***************************************"
+        "****************************\n");
     return 0;
 }
 
 int main(int argc, char* argv[])
 {
-    printf("Sample#1: Basic Usage of CLIUTILS with "
-        "One Argument and One Option.\n");
-
     zclk_command *cmd = new_zclk_command(argv[0], "cmd",
                             "Basic Description", &basic_handler);
     
@@ -40,6 +48,4 @@ int main(int argc, char* argv[])
             "Argument One"));
     
     zclk_command_exec(cmd, argc, argv);
-
-    printf("Created app successfully!\n");
 }
