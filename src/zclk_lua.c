9495e19764e1a13216a850801d730afcbbb73d5c
Abhishek Mishra <abhishekmishra3@gmail.com>
2023-01-23 23:55:18 +0530

    added initial lua module for zclk

---
+++ src/zclk_lua.c
@@ -0,0 +1,59 @@
+#include "zclk_lua.h"
+#include "zclk.h"
+
+static int zclk_command_free(lua_State *L)
+{
+    zclk_command *cmd = (zclk_command*)luaL_checkudata(L, 1, LUA_ZCLK_COMMAND_OBJECT);
+    free_command(cmd);
+    return 0;
+}
+
+static int zclk_command_new(lua_State *L)
+{
+    char* name = luaL_checkstring(L, lua_gettop(L));
+    lua_pop(L, 1);
+    char* short_name = luaL_checkstring(L, lua_gettop(L));
+    lua_pop(L, 1);
+    char* desc = luaL_checkstring(L, lua_gettop(L));
+    lua_pop(L, 1);
+    
+    //TODO: get lua command handler function and use it here.
+    zclk_command *cmd = new_zclk_command(name, short_name, desc, NULL);
+    
+    zclk_command** cmdptr = lua_newuserdata(L, sizeof(zclk_command*));
+    (*cmdptr) = cmd;
+
+    // set metatable of zclk_command object
+    luaL_getmetatable(L, LUA_ZCLK_COMMAND_OBJECT);
+    lua_setmetatable(L, -2);
+
+    return 1;
+}
+
+static const luaL_Reg ZclkCommand_funcs[] =
+{
+    {"new", zclk_command_new},
+    {NULL, NULL}
+};
+
+static const luaL_Reg ZclkCommand_meths[] =
+{
+    {"__gc", zclk_command_free},
+    {NULL, NULL}
+};
+
+int luaopen_zclk(lua_State* L)
+{
+    // create turtle metatable
+    luaL_newmetatable(L, LUA_ZCLK_COMMAND_OBJECT);
+
+    // metatable.__index = metatable
+    lua_pushvalue(L, -1);
+    lua_setfield(L, -2, "__index");
+
+    // register methods
+    luaL_setfuncs(L, ZclkCommand_meths, 0);
+    
+    // register functions - only turtle.new
+    luaL_newlib(L, ZclkCommand_funcs);
+}
---
+++ src/zclk_lua.h
@@ -0,0 +1,14 @@
+#ifndef __ZCLK_LUA_H__
+#define __ZCLK_LUA_H__
+
+#include "zclk_common.h"
+
+#include "lua.h"
+#include "lauxlib.h"
+#include "lualib.h"
+
+#define LUA_ZCLK_COMMAND_OBJECT "ZclkCommand"
+
+MODULE_API int luaopen_zclk(lua_State* L);
+
+#endif //__ZCLK_LUA_H__
